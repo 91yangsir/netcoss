6e9c2d311396c52e2f8be14bbd639231c85c3131
Administrator <Administrator@WIN-7LK575BR663>
2018-06-14 16:32:40 +0800

    gengixndwe

--- project/src/main/java/com/yangsir/project/beans/RoleBean.java
+++ project/src/main/java/com/yangsir/project/beans/RoleBean.java
@@ -2,26 +2,42 @@ package com.yangsir.project.beans;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
 /**
  * @author xiongbing
  * @version 1.0
  * @created 
  */
+@Entity
+@Table(name="t_role")
 public class RoleBean implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 5081056177957728087L;
+	@Id
+	@Column(name="id")
+	@GenericGenerator(name="hibernate.id",strategy="identity")
+	@GeneratedValue(generator="hibernate.id")
 	private long id;
 	/**
 	 *  rolenName  角色名称
 	 */
+	@Column(name="role_name",length=20)
 	private String rolenName;
 	/**
 	 *  roleType 角色类型   1--超级管理员 2--普通管理员  3--用户
 	 */
-	private int  roleType;//角色类型   1--超级管理员 2--普通管理员  3--用户
+	@Column(name="role_type",length=20)
+	private int  roleType;
 	
 	public RoleBean() {
 		// TODO Auto-generated constructor stub
--- project/src/main/java/com/yangsir/project/beans/ServerBean.java
+++ project/src/main/java/com/yangsir/project/beans/ServerBean.java
@@ -1,19 +1,75 @@
 package com.yangsir.project.beans;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 /**
  * @author Administrator
  * @version 1.0
- * @created 13-6��-2018 16:34:42
+ * @created 13-6��-2018 16:34:42
  */
-public class ServerBean {
+@Entity
+@Table(name="t_server")
+public class ServerBean implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name="id")
+	@GenericGenerator(name="hibernate.id",strategy="identity")
+	@GeneratedValue(generator="hibernate.id")
+	private  int id;
+	
 
+
+	/**
+	 * 服务器的IP地址
+	 */
+	@Column(name="server_ip",length=20)
+	private String serverIp;
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 	public ServerBean(){
 
 	}
+	
 
-	public void finalize() throws Throwable {
-
+	public String getServerIp() {
+		return serverIp;
 	}
+
+
+	public void setServerIp(String serverIp) {
+		this.serverIp = serverIp;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ServerBean [id=" + id + ", serverIp=" + serverIp + "]";
+	}
+
+
+	
+
+
 
 }
