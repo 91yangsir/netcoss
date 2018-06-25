521c8f80b26a57a707b703844084ec5398080764
Administrator <Administrator@B11E3OG4VWKSGO4>
2018-06-25 09:08:10 +0800

    资费js

--- project/src/main/webapp/static/js/page1.js
+++ project/src/main/webapp/static/js/page1.js
@@ -1,15 +1,99 @@
 $(function() {
-	getData();
-	
+	/*修改页面*/
+	$("#updateCost").bind('click',function(){
+		var row = $("#dg").datagrid('getSelected');
+		if(!row){
+			$.messager.show({
+				title:'提示',
+				msg:'请选择需要修改的数据',
+				timeout:5000,
+				showType:'slide'
+			})
+		}else{
+			$("#update_dialog").dialog('open');
+			update(row);
+		}
+	});
+
+	/*按钮确定*/
+	function update(row){
+		$("#update_form_info").bind('click',function(){
+			var data = {
+				id : row.id,
+				costName:$("#update_costName").val(),
+				costType:$("#update_costType").val(),
+				costTime:$("#update_costTime").val(),
+				costBase:$("#update_costBase").val(),
+				costUnit:$("#update_costUnit").val(),
+				costExplain:$("#update_costExplain").val()
+			};
+			console.log(data.id);
+			$.ajax({
+				type:"PUT",
+				data : data,
+				url:"/project/cost/update?id="+data.id+"&costName="+data.costName+"&costType="+data.costType+"&costTime="+data.costTime+"&costBase="+data.costBase+"&costUnit="+data.costUnit+"&costExplain="+data.costExplain+"",
+				datatype:"json",
+					success:function(msg){
+						$.messager.show({
+						title:'提示',
+						msg:'修改资费成功',
+						timeout:5000,
+						showType:'slide'
+					})
+				}
+			});
+			$("#update_dialog").dialog('close');
+			getData();
+			$("#update_costName").val('');
+			$("#update_costType").val(1);
+			$("#update_costTime").val('');
+			$("#update_costBase").val('');
+			$("#update_costUnit").val('');
+			$("#update_costExplain").val('')
+		})
+	}
+
 	/*添加页面*/
 	$("#saveCost").bind('click',function(){
 		$("#save_dialog").dialog('open');
 	})
 	/*按钮确定*/
-	$("#save_form_info").click(function(){
+	$("#save_form_info").bind('click',function(){
+		var data = {
+			costName:$("#save_costName").val(),
+			costType:$("#save_costType").val(),
+			costTime:$("#save_costTime").val(),
+			costBase:$("#save_costBase").val(),
+			costUnit:$("#save_costUnit").val(),
+			costExplain:$("#save_costExplain").val(),
+		};
+		$.ajax({
+			type:"POST",
+			data:data,
+			url:'/project/cost/save',
+			datatype:"json",
+			success:function(msg){
+				$.messager.show({
+					title:'提示',
+					msg:'添加资费成功',
+					timeout:5000,
+					showType:'slide'
+				})
+			}
+		});
 		$("#save_dialog").dialog('close');
+		getData();
+		$("#save_costName").val('');
+		$("#save_costType").val(1);
+		$("#save_costTime").val('');
+		$("#save_costBase").val('');
+		$("#save_costUnit").val('');
+		$("#save_costExplain").val('')
 	})
 
+	
+	
+	
 	/*删除页面*/
 	$("#deleteCost").bind('click',function(){
 		var row = $("#dg").datagrid('getSelected');
@@ -29,6 +113,9 @@ $(function() {
 		$("#delete_dialog").dialog('close');
 	})
 
+	
+	
+	
 	/*开通页面*/
 	$("#openCost").bind('click',function(){
 		var row = $("#dg").datagrid('getSelected');
@@ -45,9 +132,14 @@ $(function() {
 	})
 	/*按钮确定*/
 	$("#openCost_form_info").click(function(){
+		
 		$("#openCost_dialog").dialog('close');
 	})
 
+	
+	
+	
+	
 	/*暂停页面*/
 	$("#closeCost").bind('click',function(){
 		var row = $("#dg").datagrid('getSelected');
@@ -66,26 +158,7 @@ $(function() {
 	$("#closeCost_form_info").click(function(){
 		$("#closeCost_dialog").dialog('close');
 	})
-
-	
 });
 
-function getData(){    
-	console.log(111)
-    /*获得初始数据显示在页面*/
-	$("#dg").datagrid({
-		url:'/project/cost/page',
-		//格式化每个字段的显示
-		columns:[[ 
-			{field:'costName',title:'资费名',width:100,align:'center'},
-			{field:'costType',title:'资费类型',width:100,align:'center'},
-			{field:'costTime',title:'基本时长',width:90,align:'center'},
-			{field:'costBase',title:'基本费用',width:90,align:'center'},
-			{field:'costUnit',title:'单位费用',width:90,align:'center'},
-			{field:'costStart',title:'开通时间',width:120,align:'center'},
-			{field:'costExplain',title:'资费说明',width:190,align:'center'} 
-			]]
-	
-	
-	});
-}
+
+
